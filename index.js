2dc99e49b5c9b98feba6eee5270052db6175bd70
unknown <renato.senstec@gmail.com>
2022-05-03 12:44:55 -0300

    version 2.0

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
 let cards = []
 
-function addCard(card){
-    cards.push(card)
-    message('display', cards)
-    game(cards)
-    
+
+const getElementById = {
+    total: document.getElementById("total"),
+    display: document.getElementById("display"),
+    play: document.getElementById("play"),
 }
 
-function drawCard(){
+const messages = {
+    win: () => getElementById['play'].textContent = "You Win 🤑",
+    lose: () => getElementById['play'].textContent = "You Lose 😱",
+    draw: () => getElementById['play'].textContent = `${cards.length >= 1 ? 'Draw another card 😏?' : 'Wanna Play?'} `,
+    total: value => getElementById['total'].textContent = `Result: ${value ? value : ''} `,
+    display: value => getElementById['display'].textContent = `Card: ${value ? value : ''}`
 
-    let randomNumber = Math.floor(Math.random() * 13) + 1
-    if(randomNumber > 10) return addCard(10) // to emulate King, Queen, Jack
-    if(randomNumber === 1) return addCard(11) // to emulate Aces
-    addCard(randomNumber)
 }
 
-function buttonManage(type, value){
+function showMessage(type, value) {
+    return messages[type](value)
+}
 
+function buttonManage(type, value) {
     document.getElementById(type).disabled = value
 }
 
-function message(message, value){
-
-    let total = document.getElementById("result")
-    let display = document.getElementById("display")
-    let play = document.getElementById("play")
-    
-    if(message === "win") return play.textContent = "You Win 🤑"
-    if(message === "lose") return play.textContent = "You Lose 😱"
-    if(message === "draw") return play.textContent = `${cards.length >= 1 ? 'Draw another card 😏?':'Wanna Play?'} `
-    if(message === "total") return total.textContent = `Result: ${value ? value : ''} `
-    if(message === "display") return display.textContent = `Card: ${value ? value : ''}`
+function drawCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) return addCard(10) // to emulate Face cards
+    if (randomNumber === 1) return addCard(11) // to emulate Aces
+    addCard(randomNumber)
 }
 
-function game(cards){
+
+function addCard(card) {
+    cards.push(card)
+    showMessage('display', cards)
+    game(cards)
+}
+
+
+function game(cards) {
     let result = sumCard(cards)
-    if (result > 21)  return message('lose'), buttonManage('draw', true)
-    if (result === 21)  return message('win'), buttonManage('draw',true)
-    return message('draw')
+    if (result > 21) return showMessage('lose'), buttonManage('draw', true)
+    if (result === 21) return showMessage('win'), buttonManage('draw', true)
+    return showMessage('draw')
 }
 
 
-function sumCard(cards){
+function sumCard(cards) {
     let result = cards.reduce((total, el) => total += el)
-    message('total', result)
-    return result   
+    showMessage('total', result)
+    return result
 }
 
-function playAgain(){
+function playAgain() {
     cards = []
-    message('total')
-    message('display')
-    message('draw')
-    buttonManage('draw',false)
+    showMessage('total')
+    showMessage('display')
+    showMessage('draw')
+    buttonManage('draw', false)
 }
 
 
